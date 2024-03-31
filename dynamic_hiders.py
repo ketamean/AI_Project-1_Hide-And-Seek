c9d22884df0fdb3413de4e9c847e52ce95493ca5
ketamean <truongthanhtoan2003@gmail.com>
2024-03-31 13:27:31 +0700

    add classes for each level

---
+++ dynamic_hiders.py
@@ -0,0 +1,9 @@
+from problem import *
+from obstacle import *
+from player import *
+
+class Level3:
+    pass
+
+class Level4:
+    pass
---
+++ static_hiders.py
@@ -0,0 +1,9 @@
+from problem import *
+from obstacle import *
+from player import *
+
+class Level1:
+    pass
+
+class Level2:
+    pass
